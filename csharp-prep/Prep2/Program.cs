a711f1b2d8bc48aa46052210ab86d7c590ae17ae
Jarom Mariscal <jaromero_marz234@outlook.com>
2023-09-18 18:22:30 -0700

    Prep 3 ready

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -11,47 +11,55 @@ class Program
         string grade = "";
 
         int cociente = 0;
-        if(gradePercentage >= 90)
+        if (gradePercentage >= 90)
         {
             grade = "A";
             cociente = gradePercentage % 90;
-            if(cociente >= 7){
+            if (cociente >= 7)
+            {
                 grade = grade + "+";
             }
-            else{
+            else
+            {
                 grade = grade + "-";
             }
         }
-        else if(gradePercentage >= 80 && gradePercentage < 90 )
+        else if (gradePercentage >= 80 && gradePercentage < 90)
         {
             grade = "B";
             cociente = gradePercentage % 80;
-            if(cociente >= 7){
+            if (cociente >= 7)
+            {
                 grade = grade + "+";
             }
-            else{
+            else
+            {
                 grade = grade + "-";
             }
         }
-        else if(gradePercentage >= 70 && gradePercentage < 80)
+        else if (gradePercentage >= 70 && gradePercentage < 80)
         {
             grade = "C";
             cociente = gradePercentage % 70;
-            if(cociente >= 7){
+            if (cociente >= 7)
+            {
                 grade = grade + "+";
             }
-            else{
+            else
+            {
                 grade = grade + "-";
             }
         }
-        else if(gradePercentage >= 60 && gradePercentage < 70)
+        else if (gradePercentage >= 60 && gradePercentage < 70)
         {
             grade = "D";
             cociente = gradePercentage % 60;
-            if(cociente >= 7){
+            if (cociente >= 7)
+            {
                 grade = grade + "+";
             }
-            else{
+            else
+            {
                 grade = grade + "-";
             }
         }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int magicNumber = new Random().Next(1, 100);
+        int compareNumber = 0;
+        int attemptsNumber = 0;
+        do
+        {
+            Console.Write("What is your guess? ");
+            compareNumber = Convert.ToInt32(Console.ReadLine());
+            attemptsNumber++;
+            if (compareNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (compareNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed it after {attemptsNumber} attempts.");
+                Console.Write("Do you want to play again?(y/n) ");
+                string response = Console.ReadLine();
+                if (response == "y")
+                {
+                    magicNumber = new Random().Next(1, 100);
+                    attemptsNumber = 0;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        } while (compareNumber != magicNumber);
+
     }
 }
