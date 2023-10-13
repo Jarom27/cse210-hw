6107ee101f3a614f1f5b57f7d3f445360d17dcea
Jarom Mariscal <jaromero_marz234@outlook.com>
2023-10-12 22:16:25 -0700

    Learning 05

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction prove1 = new Fraction(1);
+        Fraction prove2 = new Fraction(5);
+        Fraction prove3 = new Fraction(3, 4);
+        Fraction prove4 = new Fraction(1, 3);
+
+        Console.WriteLine(prove1.GetFractionString());
+        Console.WriteLine(prove1.GetDecimalValue());
+        Console.WriteLine(prove2.GetFractionString());
+        Console.WriteLine(prove2.GetDecimalValue());
+        Console.WriteLine(prove3.GetFractionString());
+        Console.WriteLine(prove3.GetDecimalValue());
+        Console.WriteLine(prove4.GetFractionString());
+        Console.WriteLine(prove4.GetDecimalValue());
     }
 }
