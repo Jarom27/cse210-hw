e11a12c17b019babb1fa5602b6088bf2807ef357
Jarom Mariscal <jaromero_marz234@outlook.com>
2023-10-23 20:47:35 -0700

    Learning Activity FInished

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Jarom Mariscal", "CSE210 Programming with classes");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Juan Ignacio", "Fractions", "Section 17", "Problems 8-9");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Winters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
