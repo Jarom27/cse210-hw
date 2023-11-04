e0674424695ec2d9dcdbd48adae30196874579cf
Jarom Mariscal <jaromero_marz234@outlook.com>
2023-11-04 15:54:44 -0700

    Breathing Activity Ready

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -7,18 +7,55 @@ class Activity
     public void DisplayStartingMessage()
     {
         Console.WriteLine($"Welcome to the {_activityName}\n\n{_description}");
+        Console.Write("How long, in seconds, would you like for your session? ");
+        _duration = int.Parse(Console.ReadLine());
     }
     public void DisplayEndingMessage()
     {
-
+        Console.WriteLine($"You have completed {_duration} seconds of the {_activityName}");
+    }
+    public void StartSpinner()
+    {
+        int cycles = 0;
+        while (cycles < 3)
+        {
+            Console.Write("/");
+            Thread.Sleep(300);
+            Console.Write("\b \b");
+            Console.Write("-");
+            Thread.Sleep(300);
+            Console.Write("\b \b");
+            Console.Write("\\");
+            Thread.Sleep(300);
+            Console.Write("\b \b");
+            Console.Write("|");
+            Console.Write("\b \b");
+            cycles++;
+        }
+        PauseSpinner();
     }
     public void PauseSpinner()
     {
+        Console.Write("\b\b");
+        Console.WriteLine();
+    }
+    public void StartCountDown(int endCount)
+    {
+        int count = endCount;
+        while (count > 0)
+        {
+
+            Console.Write("\b \b");
+            Console.Write(count);
+            count--;
+            Thread.Sleep(1000);
+        }
+        PauseCountDown();
 
     }
     public void PauseCountDown()
     {
-
+        Console.Write("\b \b");
     }
     public virtual void Run()
     {
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -8,30 +8,30 @@ class Program
     {
 
         string option;
+
         do
         {
-            Activity activity;
+
             Console.WriteLine("Menu Options:\n1.Start breathing activity\n2.Start listening activity\n3.Start reflection activity\n4.Quit");
             option = Console.ReadLine();
-            switch (option)
+            Activity activity = null;
+            if (option == "1")
             {
-                case "1":
-                    activity = new BreathingActivity();
-                    activity.DisplayStartingMessage();
-                    activity.Run();
-                    break;
-                case "2":
-                    activity = new ReflectionActivity();
-                    activity.DisplayStartingMessage();
-                    activity.Run();
-                    break;
-                case "3":
-                    activity = new ListeningActivity();
-                    activity.Run();
-                    break;
-                default:
-                    break;
+                activity = new BreathingActivity();
             }
+            else if (option == "2")
+            {
+                activity = new ListeningActivity();
+            }
+            else if (option == "3")
+            {
+                activity = new ReflectionActivity();
+            }
+            else
+            {
+                break;
+            }
+            activity.Run();
 
         } while (option != "4");
     }
