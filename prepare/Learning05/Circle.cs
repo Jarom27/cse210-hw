5e5c384b1b675c7fd386e8db4e0ee7e31d2c4c67
Jarom Mariscal <jaromero_marz234@outlook.com>
2023-11-07 22:15:04 -0700

    Learning 5 finished

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,12 @@
+public class Circle : Shape
+{
+    private float _radius;
+    public Circle(string color, float radius) : base(color)
+    {
+        _radius = radius;
+    }
+    public override double GetArea()
+    {
+        return Math.PI * (_radius * _radius);
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("blue", 4);
+        Console.WriteLine($"{square.GetColor()} {square.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("Red", 4, 5);
+        Console.WriteLine($"{rectangle.GetColor()} {rectangle.GetArea()}");
+
+        Circle circle = new Circle("Green", 10);
+
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("orange", 10));
+        shapes.Add(new Rectangle("pink", 3, 5));
+        shapes.Add(new Circle("brown", 7));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"{shape.GetColor()} {shape.GetArea()}");
+        }
     }
 }
